09d19199971781b64db1945d179db1ee7fa14bc6
Michael McDermott (CORP) <michael.mcdermott@adp.com>
2021-02-08 12:29:29 -0500

    fix: disable firebase when testing

--- ios/DetoxTestApp/AppDelegate.m
+++ ios/DetoxTestApp/AppDelegate.m
@@ -31,10 +31,12 @@ static void InitializeFlipper(UIApplication *application) {
 #ifdef FB_SONARKIT_ENABLED
   InitializeFlipper(application);
 #endif
-  
+
+#ifndef IS_E2E_TESTING
   if ([FIRApp defaultApp] == nil) {
     [FIRApp configure];
   }
+#endif
 
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
